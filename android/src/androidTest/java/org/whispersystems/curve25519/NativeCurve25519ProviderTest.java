2d785b976ddfb225ed95803f832503cfdbdf0baf
Trevor Perrin <github@trevp.net>
2017-06-19 17:06:57 +0000

    Generalized EdDSA, new VRF

--- android/src/androidTest/java/org/whispersystems/curve25519/NativeCurve25519ProviderTest.java
+++ android/src/androidTest/java/org/whispersystems/curve25519/NativeCurve25519ProviderTest.java
@@ -1,8 +1,16 @@
 package org.whispersystems.curve25519;
 
-public class NativeCurve25519ProviderTest extends Curve25519ProviderTest {
+public class NativeCurve25519ProviderTest extends Curve25519ProviderTest implements Curve25519ProviderInternalTest {
   @Override
   protected Curve25519Provider createProvider() throws NoSuchProviderException {
     return new NativeCurve25519Provider();
   }
+
+  public native boolean internalTest();
+
+  public void testInternalTest() throws Exception {
+    assertTrue(internalTest());
+  }
+
+
 }
---
+++ tests/src/main/java/org/whispersystems/curve25519/Curve25519ProviderInternalTest.java
@@ -0,0 +1,8 @@
+package org.whispersystems.curve25519;
+
+
+public interface Curve25519ProviderInternalTest {
+
+  public abstract boolean internalTest();
+
+}
