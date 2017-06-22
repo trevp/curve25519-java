43301414782d31a3948524f22a15ef4ca3a7eec5
Trevor Perrin <github@trevp.net>
2017-06-22 07:45:58 +0000

    Trying to compile, simpler

--- android/src/androidTest/java/org/whispersystems/curve25519/NativeCurve25519ProviderTest.java
+++ android/src/androidTest/java/org/whispersystems/curve25519/NativeCurve25519ProviderTest.java
@@ -1,6 +1,6 @@
 package org.whispersystems.curve25519;
 
-public class NativeCurve25519ProviderTest extends Curve25519ProviderTest implements Curve25519ProviderInternalTest {
+public class NativeCurve25519ProviderTest extends Curve25519ProviderTest {
   @Override
   protected Curve25519Provider createProvider() throws NoSuchProviderException {
     return new NativeCurve25519Provider();
--- tests/src/main/java/org/whispersystems/curve25519/Curve25519ProviderInternalTest.java
+++
@@ -1,8 +0,0 @@
-package org.whispersystems.curve25519;
-
-
-public interface Curve25519ProviderInternalTest {
-
-  public abstract boolean internalTest();
-
-}
